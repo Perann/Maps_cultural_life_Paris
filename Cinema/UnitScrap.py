d6b34e17b32e8130f392f80182602a7787e21128
Perann <nedjar.prn@gmail.com>
2023-12-12 15:33:30 +0100

    First successful cinema scrap

--- Cinema/UnitScrap.py
+++ Cinema/UnitScrap.py
@@ -1,30 +1,38 @@
 import bs4
 import requests
+import pandas as pd
 
 HEADERS_IOS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
     
-r = requests.get('https://www.cinefil.com/cinema/ugc-cine-cite-les-halles-paris/programmation', headers= HEADERS_IOS)
-soup = bs4.BeautifulSoup(r.content,'lxml')
+url1 = 'https://www.cinefil.com/cinema/ugc-cine-cite-les-halles-paris/programmation'
 
-Database = {}
-Database['nom']  = []
-Database['heure'] = []
-Database['date'] = []
-Database['etablissement'] =[]
-Database['adresse'] = []
-Database['prix'] = []
+def UnitScrapCinema(url, date, cinema,HEADERS = HEADERS_IOS):
+    r = requests.get(url, headers= HEADERS_IOS)
+    soup = bs4.BeautifulSoup(r.content,'lxml')
 
-InfoFilm = soup.find_all('div', class_ = 'fiche-film')
+    Database = {}
+    Database['nom'] = []
+    Database['heure'] = []
 
+    InfoFilm = soup.find_all('div', class_ = 'fiche-film')
+    for info in InfoFilm:
+        title = info.find('div', class_ = 'fiche-infos').find('a').text
+        sceances = info.find('div', class_ = 'horaires').find_all('div')[2]
+        times = sceances.find_all('div', class_ = 'h4')
+        for time in times:
+            Database['heure'].append(time.text)
+            Database['nom'].append(title)
+    
+    print(Database)
+    Database['date'] = [date]*len(Database['nom'])
+    Database['etablissement'] = [cinema]*len(Database['nom'])
+    Database['adresse'] = ['adresse']*len(Database['nom'])  
+    Database['prix'] = ['variable']*len(Database['nom'])
+    
 
-for info in InfoFilm:
-    title = info.find('div', class_ = 'fiche-infos').find('a').text
-    Database['nom'].append(title)
-    sceances = info.find('div', class_ = 'horaires').find_all('div')[2]
-    times = sceances.find_all('div', class_ = 'h4')
-    for time in times:
-        Database['heure'].append(time.text)
-        Database['nom'].append(title)
-print(Database)
+    return Database
 
-
+if __name__ == '__main__':
+    DataUgc = pd.DataFrame(UnitScrapCinema(url1,'ajd','ugc chatelet'))
+    print(DataUgc.head())
+    
