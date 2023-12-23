2e6f8a7cd510f2d1e1ba67794f6965262b5691cd
Perann <nedjar.prn@gmail.com>
2023-12-23 17:12:37 +0100

    Reorg

--- Cinema/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-ref = pd.read_csv('Python_2A\Cinema\ListeCinema(MairieDeParis).csv', sep = ';')
-
-ref.rename(columns={'nom': 'etablissement'}, inplace=True)
-
-print(ref.keys())
