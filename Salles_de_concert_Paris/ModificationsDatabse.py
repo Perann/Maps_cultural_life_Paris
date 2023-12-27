6926621c3959762db59c8f27caa3c8634b5a1090
nbiviano <nicolas.biviano@ensae.fr>
2023-12-27 12:40:22 +0000

    clean database

--- Salles_de_concert_Paris/ModificationsDatabse.py
+++ Salles_de_concert_Paris/ModificationsDatabse.py
@@ -5,7 +5,7 @@ import Fonctions as fct
 
 df = pd.read_csv('DataMusiqueClassique.csv')
 
-df['adresse'] = df['adresse'] + 'Paris, France'
+df['adresse'] = df['adresse'] + ', Paris, France'
 df['coordonnees'] = df['adresse'].apply(fct.obtenir_coordonnees_adresse) 
 
 df = df.dropna(subset=['coordonnees']).reset_index(drop=True)
