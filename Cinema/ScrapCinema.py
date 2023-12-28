885625f9779f9096a6ce5acfa56e191eb18e3c59
Perann <nedjar.prn@gmail.com>
2023-12-28 11:19:30 +0100

    reorga

--- Cinema/ScrapCinema.py
+++ Cinema/ScrapCinema.py
@@ -5,10 +5,14 @@ import lxml
 import numpy as np
 import requests
 import unidecode
-from Maps_cultural_life_Paris.Cinema.UnitScrap import UnitScrapCinema
+from UnitScrap import UnitScrapCinema
 
+
+#Defining a Header to Datascrap (The site doesn't allow python request)
 HeadersDefault = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-#Taking all the links of the pages
+
+
+
 
 def ScrapCinema(url,day,HEADERS = HeadersDefault):
 
@@ -24,21 +28,23 @@ def ScrapCinema(url,day,HEADERS = HeadersDefault):
         link = html.get('href')
         LinksCinema[name] = link
 
-    dfList = []                                           #Adding each Dataframe from pages
+    dfList = []                                                  #Concatenate each Dataframe from pages
     for name_cinema in LinksCinema.keys():
         dfList.append(pd.DataFrame(UnitScrapCinema(LinksCinema[name_cinema],day, name_cinema)))
 
     DataCinema = pd.concat(dfList, axis=0, ignore_index=True)    #Creating an only DataFrame
 
-    ref = pd.read_csv('Maps_cultural_life_Paris/Cinema/ListeCinema.csv', sep = ';')          #Merging with an opendata list to get adress
+    ref = pd.read_csv('Ressources\Data\ListeCinema.csv', sep = ';')          #Merging with an opendata list to get the coordinates
+
     ref.rename(columns={'nom': 'etablissement'}, inplace=True)
     merged_df = pd.merge(DataCinema, ref, on ='etablissement', how='left')
     columns_to_drop = ['ndeg_auto','dep','region_administrative','code_insee','situation_geographique','ecrans','fauteuils','tranche_d_entrees','proprietaire','programmateur','categorie_art_et_essai','label_art_et_essai','genre','multiplexe','population_de_la_commune_2015','unite_urbaine_2010','population_unite_urbaine_2015','semaines_d_activite_2020','seances_2020','entrees_2020','entrees_2019','evolution_entrees_2020_2019','art_et_essai','nombre_de_films_programmes_2020','nombre_de_films_inedits_2020','nombre_de_films_en_semaine_1_2020','pdm_en_entrees_des_films_francais_2020','pdm_en_entrees_des_films_americains_2020','pdm_en_entrees_des_films_europeens_2020','pdm_en_entrees_des_autres_films_2020','films_art_et_essai_2020','part_des_seances_de_films_art_et_essai_2020','pdm_en_entrees_des_films_art_et_essai_2020']
     merged_df.drop(columns = columns_to_drop, axis = 1, inplace= True)
     merged_df = merged_df.dropna()
-    merged_df.to_csv('Maps_cultural_life_Paris/Outputs/DataSets/DataCinema.csv', index=False)
+    merged_df.to_csv('Outputs/DataSets/DataCinema.csv', index=False)
     return merged_df
+
 
 if __name__ == '__main__':
     url = 'https://www.cinefil.com/seances-cinema/paris'
-    ScrapCinema(url,'27/12')
+    ScrapCinema(url,'28/12')
