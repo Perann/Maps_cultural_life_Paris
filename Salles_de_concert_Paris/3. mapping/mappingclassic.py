30e84bcd904e3c935ea0fb86646541f10d2f62c9
nbiviano <nicolas.biviano@ensae.fr>
2023-12-29 10:54:07 +0000

    la map !!!!!

--- Salles_de_concert_Paris/3. mapping/mappingclassic.py
+++ Salles_de_concert_Paris/3. mapping/mappingclassic.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import folium
 import webbrowser
 
+
 def ClassicalMap(data:'Pandas DataFrame',date:'YYYY-MM-DD'):
 
     ## on convertit la date de l'utilisateur au format datetime 
@@ -17,8 +18,6 @@ def ClassicalMap(data:'Pandas DataFrame',date:'YYYY-MM-DD'):
     
     filtered_data = data[(data['date datetime'] == user_date)]
 
-    print(filtered_data)
-
 
     paris_coordinates = [48.8566, 2.3522]
     my_map = folium.Map(location=paris_coordinates, zoom_start=12)
@@ -28,9 +27,11 @@ def ClassicalMap(data:'Pandas DataFrame',date:'YYYY-MM-DD'):
         address = row['adresse']
         show_name = row['nom']
         average_price = row['prix moyen']
-
+        time = row['heure']
+        
         coordinates = [float(coord.strip('()')) for coord in row['coordonnees'].split(',')]
-        popup_text = f"<b>{establishment_name}</b><br>Adresse: {address}<br>Pièce: {show_name}<br>Prix moyen: {average_price} €"
+
+        popup_text = f"<b>{establishment_name}</b><br>Adresse: {address}<br>Concert: {show_name}<br>Heure: {time}<br>Prix moyen: {average_price} €"
         folium.Marker(location=coordinates, popup=popup_text).add_to(my_map)
     
     my_map.save("Outputs\Maps\Classicmap.html")
@@ -40,5 +41,5 @@ def ClassicalMap(data:'Pandas DataFrame',date:'YYYY-MM-DD'):
 
 #Example: Display all theater pieces played the 29th December 2023 in Paris
 if __name__ == '__main__':
-    DataClassical = pd.read_csv('Outputs\DataSets\DataMusiqueClassique_v7.csv', sep=',')
+    DataClassical = pd.read_csv('Outputs\DataSets\DataMusiqueClassique_v8.csv', sep=',')
     ClassicalMap(DataTheater,'2023-12-29')
